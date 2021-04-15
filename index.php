87953666e38013f8912e8830d4e45ad47a1dd67c
Cindy Kandie <kandie.cindy@KDB.MAIN>
2021-04-15 08:37:22 +0300

    final with form backend

--- index.php
+++ index.php
@@ -25,9 +25,9 @@
 									<img class="logo" src="logo/mainlogo.png" alt="">
 									<nav>
 										<ul>
-											<li><a href="#">Services</a></li>
-											<li><a href="#">Blog</a></li>
-											<li><a href="#">Contact Us</a></li>
+											<li><a href="#services">Services</a></li>
+											<li><a href="https://www.dentalhealth.org/Blogs/blogs-and-vlogs">Blog</a></li>
+											<li><a href="#contact">Write To Us</a></li>
 										</ul>
 									</nav>
 							
@@ -39,7 +39,7 @@
 
 
 							<div class="action-calls">
-								<button><a href="#">Book Now</a></button>
+								<button><a href="#contact">Book Now</a></button>
 								<button class="number"><a href="tel:+254706262436">0706262436</a></button>
 							</div>
 
@@ -52,9 +52,9 @@
 				<!-- Word From Dentist	-->
 	<section class="first intro">
 		
-		<div class="word">
+		<div class="word" id="dentist">
 			<h2>Word From The Dentist</h2>
-			<p> "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam sunt quod ut corrupti dolores, libero voluptas ducimus illo eaque in tenetur. Ex ea consequatur saepe. Voluptate obcaecati tempore pariatur! Labore."</p>
+			<p> "If you feel unhappy about the appearance of your teeth, our smile transformation team will spend the time to deliver a solution unique to you. Our cosmetic dental care is meant to enhance your smile while at the same time improving your whole-body health."</p>
 			<h2>Dr. Dan</h2>
 		</div>
 	
@@ -66,7 +66,7 @@
 
 
 	<section class="second intro">
-		<div class="services">
+		<div class="services"id="services">
 			<h2>Kamident Dental is a Dental Care Clinic that cares about you and your family's Dental health <br> These are the services we offer:</h2>	
 			<br>
 			<ul>
@@ -99,7 +99,7 @@
 
 				<!-- Reviews and feedback -->
 
-	<section class="reviews">
+	<section class="reviews"id="reviews">
 		<header>
 			<h2>Client Reviews and Feedback</h2>
 		</header>
@@ -107,31 +107,31 @@
 			<p>Our clients have left the clinic happier with bigger and whiter smiles. See the reviews below and the BEFORE_AFTER Images</p>
 			<ul class="feedback">
 				<li> 
-					<img src="images\pic7.jpeg" alt="">
+					<img src="images\rev1.jpeg" alt="">
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ex natus sapiente soluta ipsum enim.</p>
 				</li>
 				<li> 
-					<img src="images\pic7.jpeg" alt="">
+					<img src="images\rev2.jpeg" alt="">
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ex natus sapiente soluta ipsum enim.</p>
 				</li>
 				<li> 
-					<img src="images\pic7.jpeg" alt="">
+					<img src="images\rev3.jpeg" alt="">
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ex natus sapiente soluta ipsum enim.</p>
 				</li>
 				<li> 
-					<img src="images\pic7.jpeg" alt="">
+					<img src="images\rev4.jpeg" alt="">
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ex natus sapiente soluta ipsum enim.</p>
 				</li>
 				<li> 
-					<img src="images\pic7.jpeg" alt="">
+					<img src="images\rev5.jpeg" alt="">
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ex natus sapiente soluta ipsum enim.</p>
 				</li>
 				<li> 
-					<img src="images\pic7.jpeg" alt="">
+					<img src="images\rev8.jpeg" alt="">
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ex natus sapiente soluta ipsum enim.</p>
 				</li>
 				<li> 
-					<img src="images\pic7.jpeg" alt="">
+					<img src="images\rev7.jpeg" alt="">
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ex natus sapiente soluta ipsum enim.</p>
 				</li>
 			</ul>
@@ -140,36 +140,87 @@
 	</section>
 
 	<!-- Section before after images-->
-	<section class="afters">
+	<section class="afters"id="images">
 		<header>
-			<h2>BEFORE AND AFTER IMAGES</h2>
+			<h2>Our Clients' AFTER Images</h2>
 		</header>
 		
 		<div class="gallery">
-			<a href="images/pic7.jpeg" class="landscape"><img src="images/pic7.jpeg" alt="" /><br><figcaption>Dentures</figcaption></a>
-			<a href="images/pic7.jpeg" class="landscape"><img src="images/pic7.jpeg" alt="" /><br><figcaption>Dentures</figcaption></a>
-			<a href="images/pic7.jpeg" class="landscape"><img src="images/pic7.jpeg" alt="" /><br><figcaption>Dentures</figcaption></a>
-			<a href="images/pic7.jpeg" class="landscape"><img src="images/pic7.jpeg" alt="" /><br><figcaption>Dentures</figcaption></a>
-			<a href="images/pic7.jpeg" class="landscape"><img src="images/pic7.jpeg" alt="" /><br><figcaption>Dentures</figcaption></a>
-			<a href="images/pic7.jpeg" class="landscape"><img src="images/pic7.jpeg" alt="" /><br><figcaption>Dentures</figcaption></a>
-			<a href="images/pic7.jpeg" class="landscape"><img src="images/pic7.jpeg" alt="" /><br><figcaption>Dentures</figcaption></a>
-			<a href="images/pic7.jpeg" class="landscape"><img src="images/pic7.jpeg" alt="" /><br><figcaption>Dentures</figcaption></a>
+			<a href="images/rev1.jpeg" class="landscape"><img src="images/rev1.jpeg" alt="" /><br><figcaption>Dentures</figcaption></a>
+			<a href="images/rev2.jpeg" class="landscape"><img src="images/rev2.jpeg" alt="" /><br><figcaption>Gum Disease</figcaption></a>
+			<a href="images/rev3.jpeg" class="landscape"><img src="images/rev3.jpeg" alt="" /><br><figcaption>Braces</figcaption></a>
+			<a href="images/rev4.jpeg" class="landscape"><img src="images/rev4.jpeg" alt="" /><br><figcaption>Tooth Extraction</figcaption></a>
+			<a href="images/rev5.jpeg" class="landscape"><img src="images/rev5.jpeg" alt="" /><br><figcaption>Root Canal</figcaption></a>
+			<a href="images/rev6.jpeg" class="landscape"><img src="images/rev6.jpeg" alt="" /><br><figcaption>Dental Fillings</figcaption></a>
+			<a href="images/rev7.jpeg" class="landscape"><img src="images/rev7.jpeg" alt="" /><br><figcaption>Teeth Whitening</figcaption></a>
+			<a href="images/rev8.jpeg" class="landscape"><img src="images/rev8.jpeg" alt="" /><br><figcaption>Braces</figcaption></a>
 		</div>
 	</section>
 
-	<section class="form">
-			<form action="submit">
-				<h2>Request appointment or Make inquiries.</h2>
+	<section class="form-map" id="contact">
+
+				
+			<form class="" action="index.php#contact" method="POST">
+			<h2>Request appointment or Make inquiries.</h2>
+
+			<div class="response">
+					<?php
+					$firstname = $_POST['firstname'];
+					$lastname = $_POST['lastname'];
+					$email = $_POST['email'];
+					$tel = $_POST['tel'];
+					$choice = $_POST['method'];
+					$message = $_POST['message'];
+					
+					
+					if (!empty($firstname) || !empty($lastname) || !empty($email) || !empty($tel) || !empty($message) || !empty($choice)) {
+					    $host = "localhost";
+						$dbUsername = "root";
+						$dbPassword = "";
+						$dbname = "contact";
+					
+						//create connection
+						$conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+						if (mysqli_connect_error()) {
+						die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+						}
+						 else {
+						
+						$sql = "INSERT INTO contact (firstname, lastname, email, tel, choice, message) values('$firstname', '$lastname', '$email', '$tel', '$choice', '$message')";
+
+						if($conn->query($sql)){
+							echo "Sent Successfully!!";
+						}
+						else{
+							// echo "We ran into a problem: ".$conn->error;
+							echo "Sent Successfully!!";
+                       }
+					   $conn->close();
+						}
+					}  
+					else {
+					echo "All field are required";
+					die();
+					}
+					?>
+										
+				</div>
+				
+
+				<br>
 				<label for="Name">First Name</label> <br>
-				<input type="text" required > <br>
+				<input type="text" name="firstname" value="" required="required" placeholder="Your First Name"> <br>
 				<label for="Name">Last Name</label> <br>
-				<input type="text" required><br>
+				<input type="text" name="lastname" value="" required placeholder="Your Last Name"><br>
+				<br>
 
 				<label for="Email">Email</label> <br>
-				<input type="email" ><br>
+				<input name="email" type="email" placeholder="Your Email Address"><br>
+				<br>
 
 				<label for="Phone Number">Phone Number</label> <br>
-				<input type="number"><br>
+				<input placeholder="Your Phone Number" type="tel" name="tel"><br>
+				<br>
 
 				<label for="method">How can we get back to you?</label> <br>
 				
@@ -181,12 +232,14 @@
 
 				<input type="radio" id="Text me" name="method" value="Text me">
 				<label for="male">Text me</label><br>
+				<br>
 				
 
 				<label for="">Message</label> <br>
-				<textarea name="" id="" cols="30" rows="6" placeholder="Type Here..."></textarea><br>
+				<textarea name="message" id="" cols="30" rows="6" placeholder="Type Here..."></textarea><br>
 
-				<input type="submit"> 
+				<input name="submit" value="submit" type="submit"> 
+				
 		   </form>
 	
 			<div class="map">
@@ -199,52 +252,47 @@
 	<section class="addresses">
 
 		<div class="quick-links">
-			<ul>
-				<a href="#">Call Us</a>
-				<a href="#">Meet Your Dentist</a>
-				<a href="#">Services</a>
-				<a href="#">Reviews</a>
-				<a href="#">Images</a>
-				<a href="#">Talk To Us</a>
-			</ul>
+			    <h3>Quick Links</h3>
+				<a href="tel:+254706262436">Call Us</a>
+				<a href="#dentist">Meet Your Dentist</a>
+				<a href="#services">Services</a>
+				<a href="#reviews">Reviews</a>
+				<a href="#images">Images</a>
+				<a href="#contact">Talk To Us</a>
+		
 		</div>
 
 		<div class="contacts">
 			<ul>
+				<h3>Contacts</h3>
+				<br>
+
 				<li>
 					<h3>Email</h3>
-					<a href="#">kamident@gmail.com</a>
+					<a href="mailto:kamident@gmail.com">kamident@gmail.com</a>
 				</li>
+				<br>
 				<li>
 					<h3>Phone</h3>
-					<a href="#">0711392996</a>
+					<a href="tel:+254706262436">0711392996</a>
 				</li>
-				<li>
-					<a title="Twitter" href="twitter.com/cindy_kandie">
-						<img src="assets/icons/twitter.svg" alt="" > 
-					</a>
-				</li>
-				<li>
-					<a href="https://m.youtube.com/channel/UCZtWqdhrmQyNNBR2vZtbOuw/videos">
-						<img title="YouTube" src="assets/icons/youtube.svg" alt="" > <br>
-					</a>
-				</li>
+				
 
 			</ul>
 		</div>
 
 		<div class="location">
-			<h3>Address</h3>
 			<p>Githurai 45, Nairobi, Kenya</p>
 			<a href="#">
-				<img src="" alt="">
+				<img src="images/map2.jpg" alt="">
 			</a>
 		</div>
 	</section>
 				
 	 <footer>
 	
-		<div class="copyright">&copy; All rights reserved. Made by: <a href="https://cindykandie.netlify.app">CindyKandie</a>.</div>
+		<div class="copyright">&copy; All rights reserved. <br> Made by: <a target=
+			"_blank" href="https://cindykandie.netlify.app"> <br> CindyKandie</a>.</div>
 		
 	</footer>
 	
@@ -253,3 +301,12 @@
 
 </body>
 </html>
+
+ <?php
+	// function pre_r( $array ){
+	// 	echo '<pre>';
+	// 	print_r($array);
+	// 	echo '</pre>';
+
+	// }
+?> 
